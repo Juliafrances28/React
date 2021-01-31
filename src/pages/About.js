a56aae0d69da0ca85ef531c1abe6a16dec9f55d4
Julia Horan <julia.horan28@gmail.com>
2021-01-30 21:58:25 -0500

    changes made

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,21 +3,16 @@ import Container from "../components/Container/index";
 import Col from "../components/Col/index";
 import Row from "../components/Row/index";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 function About() {
   return (
 
     <div>
-      <Container />
-        <Row>
-          <Col size="md-12">
-            <h1>Welcome To Pupster!</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <p>
+      <Container>
+        <Col>
+        <h1>Welcome To Pupster!</h1>
+        </Col>
+        <Row> 
+           <p>
               March 2020 I found myself traveling down an unfamiliar track when
               my job at the MTA ended due to Covid. I asked myself, “What would
               be a valuable new skill to help my career thrive during and after
@@ -27,8 +22,9 @@ function About() {
               was fun and engaging. I was grateful to be able to return to my
               childhood home in New Jersey and in September I began the
               six-month coding bootcamp at Rutgers University.
-            <p>
-
+            </p>
+            </Row>
+            <Row>
             <p>
               Turns out that coding is very much like a hobby I’ve enjoyed for
               many years, jigsaw puzzling. I always have one going on my coffee
@@ -39,9 +35,8 @@ function About() {
               a challenging jigsaw puzzle feels a lot like the satisfaction of
               seeing the code “work.”
             </p>
-          <Col>
-        <Row>
-        <Container />
+            </Row>
+            </Container>
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,27 @@
-import React from "react";
+iimport React, { Component } from "react";
+import ReactDom from "react-dom";
 import Wrapper from "../components/Wrapper/index";
 import Nav from "../components/Nav/index";
 import Card from "../components/Card/index";
 import Footer from "../components/Footer/index"; 
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
-function Contact(props) {
+function Contact() {
   return (
-    <div>
-     
-      <Wrapper />
-      <Nav />
-      <Card />
+     <div>   
+     <Nav />
+      <Wrapper>
+      <Card>
 
       <h5 class="card-header" id = "Contact" >Julia Frances Horan</h5>
 
         <p class="card-text"></p>
 
-         <ul class="list-unstyled"> 
+         <ul class="list-unstyled" /> 
         <li> Phone: 908-489-7803</li>
         <li> Email: Julia.horan28@gmail.com </li>
         <li> <a href = "https://www.linkedin.com/in/juliafhoran/" > Linkedin </a></li>
         <li> <a href = "https://github.com/Juliafrances28"> github </a></li>
-       <ul> 
+       <ul /> 
 
          <h5 class="card-title"> I look forward to hearing from you!!</h5>
          
@@ -31,11 +29,12 @@ function Contact(props) {
     src="https://www.eu-startups.com/wp-content/uploads/2020/06/woman-coding.jpg"
     width="50%;" />
 
-      <Card />
-      <Wrapper  />
+      </Card>
+      </Wrapper>
       <Footer />
-    </div>
+    
   );
-}
+  
+  }; 
 
-export default Contact;
+export default Contact; 
