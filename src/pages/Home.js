87709698c4d873f2e78017b03d9f1ab6349a8492
Julia Horan <julia.horan28@gmail.com>
2021-05-20 11:43:25 -0400

    changes to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero/index";
+import Row from "../components/row/index";
 import "./style.css";
 
 function Home() {
@@ -9,6 +10,20 @@ function Home() {
         <h1> Julia Frances Horan </h1>
         <h2> Front End Web Developer </h2>
       </Hero>
+      <div>
+        <Row>
+          <h4> 908-489-7803 </h4>
+          <h4> julia.horan28@gmail.com </h4>
+          <h4>
+            {" "}
+            <a href="https://www.linkedin.com/in/juliafhoran/"> Linkedin </a>
+          </h4>
+          <h4>
+            {" "}
+            <a href="https://github.com/Juliafrances28"> github </a>{" "}
+          </h4>
+        </Row>
+      </div>
     </div>
   );
 }
